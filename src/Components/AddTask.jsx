c008de5213bb4c003e1e010589424eb30a54d588
Dev Team <dev@coursevita.com>
2024-07-10 09:05:56 +0530

    state management

--- src/Components/AddTask.jsx
+++ src/Components/AddTask.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux";
+import { increment, decrement, incrementByData, addTask, deleteTask } from "../Features/taskSlice";
 
 
 
@@ -7,8 +9,27 @@ const AddTask = () => {
 
     const [task, setTask] = useState("");
 
+    const value = useSelector((state) => state.taskSlice.value);
+    const abc = useSelector(state => state.taskSlice.abc);
+
+    const tasks = useSelector(state => state.taskSlice.task)
+    const dispatch = useDispatch();
+    const [customValue, setCustomValue] = useState();
+
+    const handleIncrement = () => {
+        dispatch(increment())
+    }
+
+    const handleDecrement = () => {
+        dispatch(decrement())
+    }
+
+    const handleValue = () => {
+        dispatch(incrementByData(Number(customValue)))
+    }
+
     const submitTask = () =>{
-        alert(task)
+        dispatch(addTask(task))
     }
 
     return (
@@ -19,6 +40,14 @@ const AddTask = () => {
         //         <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Finish my homework" />
         //     </div>
         // </div>
+        <>
+        <h1>Value: {value}</h1>
+        <h1>ABC: {abc}</h1>
+        <button onClick={handleIncrement}>Increment</button>
+        <button onClick={handleDecrement}>Decrement</button>
+        <br/>
+        <input onChange={(event) => setCustomValue(event.target.value)}/>
+        <button onClick={handleValue}>Custom</button>
         <form className="row g-3" onSubmit={submitTask}>
             <div className="col-auto">
                 <input type="text" onChange={(event) => setTask(event.target.value)} className="form-control" id="task" placeholder="Task"/>
@@ -26,7 +55,16 @@ const AddTask = () => {
             <div className="col-auto">
                 <button type="submit" className="btn btn-primary mb-3">Add Task</button>
             </div>
+            
         </form>
+        <div>
+                {tasks.map((task, idx) =><div key={idx}>
+                        <h2 >{task}</h2>
+                        <button onClick={() => dispatch(deleteTask(idx))} className="btn btn-danger mb-3">Remove</button>
+                    </div>
+                )}
+            </div>
+        </>
     )
 }
 
--- src/Features/taskSlice.js
+++ src/Features/taskSlice.js
@@ -1,6 +1,34 @@
 import { createSlice } from "@reduxjs/toolkit";
 
+const initialState = {
+    value: 3456,
+    abc: 6789,
+    task: ['First Task', 'Second Task']
+}
+
 
 export const taskSlice = createSlice({
-    
-})
+    name: 'taskSlice',
+    initialState,
+    reducers: {
+        increment: (state) => {
+          state.value += 1
+        },
+        decrement: (state) => {
+          state.value -= 1;
+        },
+        incrementByData: (state, action) =>{
+          state.value += action.payload
+        },
+        addTask: (state, action) => {
+          state.task.push(action.payload);
+        },
+        deleteTask: (state, action) => {
+            state.task.splice(action.payload, 1);
+        }
+    }
+});
+
+export const { increment, decrement, incrementByData, addTask, deleteTask } = taskSlice.actions;
+
+export default taskSlice.reducer;
